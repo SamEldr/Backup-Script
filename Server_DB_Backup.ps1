c782b85cba46da0bde8132cefcf579e7b0acac7f
Sam Eldridge <sam@ez911inc.net>
2022-07-13 10:45:33 -0500

    First commit
    First commit

---
+++ Server_DB_Backup.ps1
@@ -0,0 +1,19 @@
+﻿$server = 'Sam-PC\SQL2019'
+$database = 'ez911Data'
+$temp_path = 'C:\Backup\temp'
+$backup_path = 'C:\Backup\ez911Data'
+
+New-Item -ItemType Directory -Force -Path C:\Backup\Temp
+New-Item -ItemType Directory -Force -Path C:\Backup\ez911Data
+
+# Backup database and compresses it. 
+
+Backup-DbaDatabase -SqlInstance $server -Database $database -Path $temp_path -Type Full -CompressBackup
+
+# Move temp folder to ez911Data folder
+
+Get-ChildItem -Path $temp_path -Recurse | Move-Item -Destination $backup_path
+
+# Filter
+
+Get-ChildItem –Path $backup_path -Recurse | Where-Object { ($_.LastWriteTime -gt (Get-Date).AddDays(-8)) -and ($_.LastWriteTime.DayOfWeek -eq 'Sunday') -and ($_.LastWriteTime -gt (Get-Date).AddDays((-22)) -and ($_.LastWriteTime.Day -eq 1) -and ($_.LastWriteTime -gt (Get-Date).AddDays(-91)) -and ($_.LastWriteTime -lt (Get-Date).AddDays(-91))) } | Remove-Item
---
+++ Server_DB_Backup_Task_Scheduler.ps1
@@ -0,0 +1,5 @@
+﻿$backup_script = 'C:\Users\seldr\Documents\GitHub\EzDataBackup\Server_DB_Backup.ps1'
+
+$Trigger= New-ScheduledTaskTrigger -At 3:00am –Daily # Specify the trigger settings
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '$backup_script' # Specify what program to run and with its parameters
+Register-ScheduledTask -TaskName "ezDataBackup" -Trigger $Trigger  -Action $Action –Force # Specify the name of the task
