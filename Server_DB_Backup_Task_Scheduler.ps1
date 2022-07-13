d7d52743abb9d53a3d361f7a439217b29c39e88b
Sam Eldridge <sam@ez911inc.net>
2022-07-13 13:18:39 -0500

    Update Server_DB_Backup_Task_Scheduler.ps1

--- Server_DB_Backup_Task_Scheduler.ps1
+++ Server_DB_Backup_Task_Scheduler.ps1
@@ -2,4 +2,4 @@
 
 $Trigger= New-ScheduledTaskTrigger -At 3:00am –Daily # Specify the trigger settings
 $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument '$backup_script' # Specify what program to run and with its parameters
-Register-ScheduledTask -TaskName "ezDataBackup" -Trigger $Trigger  -Action $Action –Force # Specify the name of the task
+Register-ScheduledTask -TaskName "ezDataBackup" -Trigger $Trigger  -Action $Action –Force
